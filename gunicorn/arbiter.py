e4dd3c428a9ac2dd4184d434dbea7d330bfb2f71
Randall Leeds <randall@bleeds.info>
2020-04-20 15:55:05 -0700

    Reap workers in the main execution context
    Handle SIGCHLD like every other signal, waking up the arbiter and
    handling the signal in the main execution context rather than in a
    signal handler context.
    
    Take special care to reinstall the signal handler in case Python avoided
    doing so to prevent infinite recursion.

--- gunicorn/arbiter.py
+++ gunicorn/arbiter.py
@@ -42,7 +42,7 @@ class Arbiter(object):
     # I love dynamic languages
     SIG_QUEUE = []
     SIGNALS = [getattr(signal, "SIG%s" % x)
-               for x in "HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH".split()]
+               for x in "CHLD HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH".split()]
     SIG_NAMES = dict(
         (getattr(signal, name), name[3:].lower()) for name in dir(signal)
         if name[:3] == "SIG" and name[3] != "_"
@@ -186,7 +186,6 @@ class Arbiter(object):
         # initialize all signals
         for s in self.SIGNALS:
             signal.signal(s, self.signal)
-        signal.signal(signal.SIGCHLD, self.handle_chld)
 
     def signal(self, sig, frame):
         if len(self.SIG_QUEUE) < 5:
@@ -237,10 +236,15 @@ class Arbiter(object):
                 self.pidfile.unlink()
             sys.exit(-1)
 
-    def handle_chld(self, sig, frame):
+    def handle_chld(self):
         "SIGCHLD handling"
         self.reap_workers()
-        self.wakeup()
+
+        # On some systems, failing to wait on a child process in a SIGCHLD
+        # handler causes infinite recursion. Python guards against this by not
+        # reinstalling the handler automatically on these systems. In case we
+        # are running on such a system, reinstall the handler explicitly.
+        signal.signal(signal.SIG_CHLD, self.signal)
 
     def handle_hup(self):
         """\
@@ -388,9 +392,11 @@ class Arbiter(object):
         limit = time.time() + self.cfg.graceful_timeout
         # instruct the workers to exit
         self.kill_workers(sig)
+        self.reap_workers()
         # wait until the graceful timeout
         while self.WORKERS and time.time() < limit:
             time.sleep(0.1)
+            self.reap_workers()
 
         self.kill_workers(signal.SIGKILL)
 
